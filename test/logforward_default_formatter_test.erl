6024834eaa940cc1a3a011017bf2408f41953c09
alking <aijingsun6@gmail.com>
2019-09-11 10:19:20 +0800

    logforward_default_formatter_test format_file_test

--- test/logforward_default_formatter_test.erl
+++ test/logforward_default_formatter_test.erl
@@ -8,12 +8,20 @@ parse_pattern_test() ->
   ?assertEqual([module], logforward_default_formatter:parse_pattern("%module")),
   ?assertEqual([datetime, " [", level, "] ", msg, eol], logforward_default_formatter:parse_pattern("%datetime [%level] %msg%eol")),
   ?assertEqual([datetime, " [", level, "]-", msg, eol], logforward_default_formatter:parse_pattern("%datetime [%level]-%msg%eol")),
-  ?assertEqual([" ", datetime, " [", level, "]-", msg, eol], logforward_default_formatter:parse_pattern(" %datetime [%level]-%msg%eol")),
-  ?assertEqual(["file.", nth, ".log"], logforward_default_formatter:parse_pattern("file.%nth.log")).
+  ?assertEqual([" ", datetime, " [", level, "]-", msg, eol], logforward_default_formatter:parse_pattern(" %datetime [%level]-%msg%eol")).
 
 format_test() ->
-  ?assertEqual("2019-09-10 12:01:02.234 [DEBUG] - abcde\r\n", logforward_default_formatter:format(#logforward_msg{datetime = {{2019, 9, 10}, {12, 1, 2}}, timestamp_ms = 1234, level = debug, format = "~p", args = [abcde]}, [datetime, " [", level, "] - ", msg, eol], [])),
+  ?assertEqual("2019-09-10 12:01:02.234 [DEBUG] - abcde\r\n",
+    logforward_default_formatter:format(#logforward_msg{datetime = {{2019, 9, 10}, {12, 1, 2}}, timestamp_ms = 1234, level = debug, format = "~p", args = [abcde]},
+      [datetime, " [", level, "] - ", msg, eol], [])),
 
-  ?assertEqual("2019-09-10 12:01:02.234 [DEBUG] - 111-abcde-222\r\n", logforward_default_formatter:format(#logforward_msg{datetime = {{2019, 9, 10}, {12, 1, 2}}, timestamp_ms = 1234, level = debug, format = "111-~p-222", args = [abcde]}, [datetime, " [", level, "] - ", msg, eol], [])),
+  ?assertEqual("2019-09-10 12:01:02.234 [DEBUG] - 111-abcde-222\r\n",
+    logforward_default_formatter:format(#logforward_msg{datetime = {{2019, 9, 10}, {12, 1, 2}}, timestamp_ms = 1234, level = debug, format = "111-~p-222", args = [abcde]},
+      [datetime, " [", level, "] - ", msg, eol], [])),
 
+  ok.
+
+format_file_test() ->
+  ?assertEqual("info.0.log", logforward_default_formatter:format_file(["info.", nth, ".log"], [{nth, 0}])),
+  ?assertEqual("info.2019-09-11.0.log", logforward_default_formatter:format_file(["info.", date, ".", nth, ".log"], [{nth, 0}, {date, {2019, 9, 11}}])),
   ok.
